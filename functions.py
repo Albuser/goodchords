7c1005862ae685c5ddf0ee9a3c7d0056e05fb5a3
Albuser <alexanderbuser@gmail.com>
2023-12-19 08:14:11 -0500

    formatting and alternate voicings

--- functions.py
+++ functions.py
@@ -4,19 +4,36 @@ import os
 parent_dir = r"C:/Users/alexa/Documents/Programming/Music/goodchord/"
 
 
-def generateChordFamily(cycle, root, scaleType, chord, fname):
+def generateChordFamily(cycle, root, scaleType, chordNotes, fname):
     scale = Scale(root, scaleType)
-    chordTones = [ChordTone(scale.notes[x]) for x in chord]
+    chordTones = [ChordTone(scale.notes[x]) for x in chordNotes]
     startingChord = Chord(scale, chordTones[0].pitchClass, chordTones)
     chord = startingChord.copy()
     permutation = chord.getBestInversion(cycle - 1)
-    while True:
-        with open(fname, "a", encoding="UTF-8") as f:
-            f.write(chord.__repr__() + "\n")
-        chord.transpose(cycle - 1)
-        chord.permute(permutation)
-        if chord.pitchClasses() == startingChord.pitchClasses():
-            break
+    with open(fname, "a", encoding="UTF-8") as f:
+        f.write("(" + " ".join([str(x + 1) for x in chordNotes]) + ") ")
+    newChordNotes = chordNotes.copy()
+    for i in range(len(permutation)):
+        newChordNotes[i] = chordNotes[permutation[i]]
+    with open(fname, "a", encoding="UTF-8") as f:
+        f.write("--> (" + " ".join([str(x + 1) for x in newChordNotes]) + ")\n")
+    indx = 0
+    while indx < len(chordTones) * len(scaleType):
+        if indx > 0:
+            with open(fname, "a", encoding="UTF-8") as f:
+                f.write("Alternate Voicing:\n")
+        while True:
+            with open(fname, "a", encoding="UTF-8") as f:
+                f.write(chord.__repr__() + "\n")
+            indx += 1
+            chord.transpose(cycle - 1)
+            chord.permute(permutation)
+            if chord.pitchClasses() == startingChord.pitchClasses():
+                break
+        startingChord = Chord(
+            scale, chordTones[1].pitchClass, chordTones[1:] + [chordTones[0]]
+        )
+        chord = startingChord.copy()
 
 
 def safeMakeFolder(path):
@@ -40,14 +57,19 @@ def write_files(root, scaleType, chordVoicings):
     safeMakeFolder(path)
     path = path + f"/{chordType}/"
     safeMakeFolder(path)
-    for chordName, chord in chordVoicings.items():
-        for cycle in range(2, 8):
-            safeMakeFolder(path + f"cycle_{cycle}/")
-            fname = path + f"cycle_{cycle}/{chordName}.txt"
-            with open(fname, "w", encoding="UTF-8") as f:
-                f.write(
-                    f"{rootName.capitalize()} {scaleName}, Cycle {cycle}, {chordName} Voicing\n"
-                )
-            generateChordFamily(
-                cycle, root=root, scaleType=scaleNotes, chord=chord, fname=fname
+    for cycle in range(2, 8):
+        fname = path + f"Cycle {cycle}.txt"
+        with open(fname, "w", encoding="UTF-8") as f:
+            f.write(
+                f"{rootName.capitalize()} {scaleName}, Cycle {cycle}\n"
+                + "-" * 25
+                + "\n"
             )
+        for chordName, chord in chordVoicings.items():
+            with open(fname, "a", encoding="UTF-8") as f:
+                f.write(f"{chordName.capitalize()}: ")
+            generateChordFamily(
+                cycle, root=root, scaleType=scaleNotes, chordNotes=chord, fname=fname
+            )
+            with open(fname, "a", encoding="UTF-8") as f:
+                f.write("-" * 25 + "\n")
